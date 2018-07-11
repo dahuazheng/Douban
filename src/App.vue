c574f9d2502ecd7cfbb8d9cc917f2bb04f70c930
dahuazheng <webdahua@163.com>
2018-07-11 16:32:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  @import "../static/iconfont/iconfont.css";
+  html, #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+  }
+  body{
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/login/Login.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="login">
+    <header :class="{focus: onKeyboard}">
+      <i class="iconfont icon-2fanhui"></i>
+      <span class="title">登录豆瓣</span>
+    </header>
+    <main>
+      <h1 v-show="!onKeyboard">欢迎来到豆瓣</h1>
+      <div class="form">
+        <input @focus="onFocus('email')"
+               @blur="onBlur('email')"
+               type="text" placeholder="手机号/邮箱">
+        <input @focus="onFocus('password')"
+               @blur="onBlur('password')"
+               type="password" placeholder="密码">
+        <button>登录</button>
+      </div>
+      <aside>
+        <span>注册豆瓣</span>
+        <span>忘记密码</span>
+      </aside>
+    </main>
+    <!--<footer>
+      <span>微信登录</span>
+      <span>微博登录</span>
+    </footer>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        userInfo: {
+          email: '',
+          password: ''
+        },
+        onKeyboard: false,
+        clientHeight: 0,
+      }
+    },
+    methods: {
+      onFocus(value) {
+
+      },
+      onBlur(value) {
+
+      },
+      openKeyboard(){
+        const nowClientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+        if (this.clientHeight > nowClientHeight) {
+          this.onKeyboard = true;
+        }
+        else {
+          this.onKeyboard = false;
+        }
+      }
+
+    },
+    mounted() {
+      this.clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+      window.addEventListener('resize', this.openKeyboard)
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  @import '../../static/less/variables';
+
+  .login {
+    background-color: #fff;
+  }
+
+  header {
+    position: relative;
+    height: @headerHeight;
+    box-sizing: border-box;
+    text-align: center;
+    line-height: @headerHeight;
+    font-size: @headerFont;
+
+    &.focus {
+      border-bottom: 1px solid #f6f6f6;
+
+      span {
+        opacity: 1;
+      }
+    }
+    span {
+      display: inline-block;
+      opacity: 0;
+      transition: all 0.25s;
+    }
+    i {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 20px;
+      margin: auto;
+      color: @themColor;
+    }
+  }
+
+  main {
+    margin-left: 20px;
+    margin-right: 20px;
+    padding-top: 30px;
+
+    h1 {
+      margin: 20px 0 50px;
+      text-align: center;
+      color: @themColor;
+      font-weight: normal;
+    }
+    .form {
+      input {
+        width: 100%;
+        height: 2rem;
+        box-sizing: border-box;
+        margin-bottom: 20px;
+        padding-left: 8px;
+        border: none;
+        border-bottom: 1px solid #ccc;
+        outline: none;
+      }
+      button {
+        width: 100%;
+        height: 40px;
+        margin-bottom: 20px;
+        border: none;
+        border-radius: 2px;
+        background-color: @themColor;
+        color: #fff;
+        outline: none;
+      }
+    }
+    aside {
+      width: 100%;
+      text-align: center;
+      font-size: 0;
+
+      span {
+        display: inline-block;
+        padding: 0 15px;
+        font-size: 1rem;
+
+        &:first-child {
+          color: @themColor;
+          border-right: 1px solid #ccc;
+        }
+      }
+    }
+  }
+</style>
